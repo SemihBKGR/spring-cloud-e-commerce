d29e16e6e8b6ce868e7a88532ed29c0c05ecf42c
SemihBG <buyukgungorsemih@gmail.com>
2021-10-16 23:59:44 +0300

    User service model instantiation, discovery service bootstrap config

--- discovery-server/src/main/java/com/semihbkgr/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/semihbkgr/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.semihbkgr.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
