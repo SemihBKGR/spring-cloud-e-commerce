4f1002347b8b4498c5b380c334ea231a26dc9204
SemihBG <buyukgungorsemih@gmail.com>
2021-10-22 23:19:36 +0300

    ui server add model common module as dependency and make openfeign apis compatible with user model classes

--- ui-server/src/main/java/com/semihbkgr/ecommerce/uiserver/client/UserClient.java
+++ ui-server/src/main/java/com/semihbkgr/ecommerce/uiserver/client/UserClient.java
@@ -1,8 +1,8 @@
 package com.semihbkgr.ecommerce.uiserver.client;
 
-import com.semihbkgr.ecommerce.uiserver.model.User;
+import com.semihbkgr.ecommerce.modelcommon.user.User;
+import com.semihbkgr.ecommerce.modelcommon.user.UserInfo;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,6 +16,6 @@ public interface UserClient {
     User getByUsername(@PathVariable("username") String username, @RequestHeader("Authorization") String authorization);
 
     @GetMapping("/search/{username}")
-    List<User> searchByUsername(@PathVariable("username") String username, @RequestHeader("Authorization") String authorization);
+    List<UserInfo> searchByUsername(@PathVariable("username") String username, @RequestHeader("Authorization") String authorization);
 
 }
--- ui-server/src/main/java/com/semihbkgr/ecommerce/uiserver/controller/UserController.java
+++ ui-server/src/main/java/com/semihbkgr/ecommerce/uiserver/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.semihbkgr.ecommerce.uiserver.controller;
 
+import com.semihbkgr.ecommerce.modelcommon.user.User;
+import com.semihbkgr.ecommerce.modelcommon.user.UserInfo;
 import com.semihbkgr.ecommerce.uiserver.client.UserClient;
-import com.semihbkgr.ecommerce.uiserver.model.User;
 import com.semihbkgr.ecommerce.uiserver.util.HeaderUtils;
 import lombok.RequiredArgsConstructor;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +21,12 @@ public class UserController {
     private final UserClient userClient;
 
     @GetMapping("/get/{username}")
-    public User getUserByUsername(@PathVariable("username")String username, HttpServletRequest request){
+    public User getUserByUsername(@PathVariable("username") String username, HttpServletRequest request) {
         return userClient.getByUsername(username, HeaderUtils.getAuthenticationHeader(request));
     }
 
     @GetMapping("/search/{username}")
-    public List<User> searchUserByUsername(@PathVariable("username")String username, HttpServletRequest request){
+    public List<UserInfo> searchUserByUsername(@PathVariable("username") String username, HttpServletRequest request) {
         return userClient.searchByUsername(username, HeaderUtils.getAuthenticationHeader(request));
     }
 
