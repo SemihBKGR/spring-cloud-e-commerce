18e051e5feda744eeebc17dc52551c39168e920b
SemihBG <buyukgungorsemih@gmail.com>
2021-10-22 23:42:02 +0300

    ui service adding thymeleaf and return html file from user controller, reviewing pom files have model common dependency by adding system scope and system path

--- ui-server/src/main/java/com/semihbkgr/ecommerce/uiserver/controller/UserController.java
+++ ui-server/src/main/java/com/semihbkgr/ecommerce/uiserver/controller/UserController.java
@@ -1,19 +1,17 @@
 package com.semihbkgr.ecommerce.uiserver.controller;
 
-import com.semihbkgr.ecommerce.modelcommon.user.User;
-import com.semihbkgr.ecommerce.modelcommon.user.UserInfo;
 import com.semihbkgr.ecommerce.uiserver.client.UserClient;
 import com.semihbkgr.ecommerce.uiserver.util.HeaderUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
@@ -21,13 +19,15 @@ public class UserController {
     private final UserClient userClient;
 
     @GetMapping("/get/{username}")
-    public User getUserByUsername(@PathVariable("username") String username, HttpServletRequest request) {
-        return userClient.getByUsername(username, HeaderUtils.getAuthenticationHeader(request));
+    public String getUserByUsername(@PathVariable("username") String username, HttpServletRequest request, Model model) {
+        model.addAttribute("user", userClient.getByUsername(username, HeaderUtils.getAuthenticationHeader(request)));
+        return "user-profile";
     }
 
     @GetMapping("/search/{username}")
-    public List<UserInfo> searchUserByUsername(@PathVariable("username") String username, HttpServletRequest request) {
-        return userClient.searchByUsername(username, HeaderUtils.getAuthenticationHeader(request));
+    public String searchUserByUsername(@PathVariable("username") String username, HttpServletRequest request, Model model) {
+        model.addAttribute("userInfoList", userClient.searchByUsername(username, HeaderUtils.getAuthenticationHeader(request)));
+        return "user-search";
     }
 
 }
